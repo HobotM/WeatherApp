25ba316914c1cca021f7ad0d468b4cf48a36c3d3
HobotM <mateuszhobot@gmail.com>
2023-07-22 22:48:24 +0100

    temperature unit changed to Celcius

--- WeatherApp/Services/WeatherService.cs
+++ WeatherApp/Services/WeatherService.cs
@@ -14,7 +14,7 @@ public class WeatherService
 
     public async Task<WeatherResponse> GetWeatherAsync(string city)
     {
-        var response = await _http.GetStringAsync($"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=0bb5173502f54f5a0f4f4e2dedb2998a");
+        var response = await _http.GetStringAsync($"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=0bb5173502f54f5a0f4f4e2dedb2998a&units=metric");
         return JsonSerializer.Deserialize<WeatherResponse>(response);
     }
 }
